455973b9cbe2bd53c479c29626da883d4ee1d981
Khannfa <30702168+Khannfa@users.noreply.github.com>
2020-02-18 02:25:05 -0800

    Create Yaml_files.py
    Python Script to Parse Yaml

---
+++ Tagging/Yaml_files.py
@@ -0,0 +1,21 @@
+import yaml
+
+def yaml_loader(filepath):
+	"""Loads a yaml file"""
+	with open(filepath, "r") as stream:
+		data = yaml.safe_load(stream)
+	return data
+
+def yaml_dump(filepath, data):
+	"""Dumps data to a yaml file"""
+	with open(filepath, "w") as stream:
+		yaml.dump(data, stream)
+
+if __name__ == "__main__":
+	filepath = "test.yaml"
+	data = yaml_loader(filepath)
+	print (data)
+
+	items = data.get('clients')
+	for item_name, item_value in items.items():
+		print (item_name, item_value)
