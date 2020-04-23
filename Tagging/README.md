9ca456e00be09fd3741d4d7245be20ee1a34ec7a
Andrew Quach <andrewquach10@gmail.com>
2020-04-23 09:47:54 -0700

    Refactor code, add dependencies to requirements, and update README

--- Tagging/README.md
+++ Tagging/README.md
@@ -1,1 +1,19 @@
-Allow to tag a Virtual machine created in VMWare VCenter. It's using a python script use with pyVmomi since it is not possible to assign a tag to a VM with the ansible module actually. It uses a YAML configuration file to store tagging info. 
+# Overview
+`tag_op.py` performs allows for the tagging/untagging of a virtual machine in vSphere. It internally uses the Python SDK for VMware vSphere. The script has currently been tested on the development environment. It is not yet runnable in production without further testing. Tagging information is stored in the yaml file.
+
+On the development server, a VM called 'Test' has been created to test the tagging feature. Two tags are currently avaliable: chargeable and managed.
+
+#### Usage:
+Usage: `python3 tag_op.py <tag/untag> <tag_name> <vm_name>`
+Ex: `python3 tag_op.py tag managed Test`
+
+#### Configuration:
+Please have a file called `settings.py` in this same directory with the following information.
+
+```python
+host = 'vctr-vd01.sig.oregonstate.edu'
+user = '<replace with real username>'
+password = '<replace with real password>'
+port = 443 
+url = 'https://{}/api'.format(host)
+```
---
+++ Tagging/tag_op.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+import ssl
+import time
+import sys
+import requests
+from com.vmware.cis.tagging_client import (Tag, TagAssociation)
+from com.vmware.cis_client import Session
+from com.vmware.vapi.std_client import DynamicID
+from pyVim.connect import Disconnect, SmartConnect
+from pyVmomi import vim
+from vmware.vapi.lib.connect import get_requests_connector
+from vmware.vapi.security.session import create_session_security_context
+from vmware.vapi.security.user_password import create_user_password_security_context
+from vmware.vapi.stdlib.client.factories import StubConfigurationFactory
+from settings import host, user, password, port, url
+
+tags = {
+        'managed': 'urn:vmomi:InventoryServiceTag:3a2841b7-495c-4b72-a1e9-9654571ba9f7:GLOBAL',
+        'chargeable' : 'urn:vmomi:InventoryServiceTag:d66e1dfc-3791-43c2-a336-0b7abd193241:GLOBAL'
+}
+
+def get_vm_id(name):
+    """Find vm id by given name using pyVmomi."""
+    context = None
+    if hasattr(ssl, '_create_unverified_context'):
+        sslContext = ssl._create_unverified_context()
+
+    si = SmartConnect(host=host, port=port, user=user, pwd=password, sslContext=sslContext)
+    content = si.content
+    container = content.rootFolder
+    viewType = [vim.VirtualMachine]
+    recursive = True
+    vmView = content.viewManager.CreateContainerView(container, viewType, recursive)
+    vms = vmView.view
+    for vm in vms:
+        if vm.name == name:
+            return vm._GetMoId()
+    raise Exception('VM with name {} could not be found'.format(name))
+
+def get_tag_association():
+    session = requests.Session()
+    session.verify = False
+    connector = get_requests_connector(session=session, url=url)
+    stub_config = StubConfigurationFactory.new_std_configuration(connector)
+
+    user_password_security_context = create_user_password_security_context(user, password)
+    stub_config.connector.set_security_context(user_password_security_context)
+
+    session_svc = Session(stub_config)
+    session_id = session_svc.create()
+
+    session_security_context = create_session_security_context(session_id)
+    stub_config.connector.set_security_context(session_security_context)
+
+    tag_association = TagAssociation(stub_config)
+    return tag_association
+
+def tag(vm_name, tag_op, tag_id):
+    # get VM
+    vm_moid = get_vm_id(vm_name)
+    assert vm_moid is not None
+
+    # get tag association
+    tag_association = get_tag_association()
+
+    if tag_op == "tag":
+        dynamic_id = DynamicID(type='VirtualMachine', id=vm_moid)
+        tag_association.attach(tag_id=tag_id, object_id=dynamic_id)
+        for tag_id in tag_association.list_attached_tags(dynamic_id):
+            if tag_id == tag_id:
+                tag_attached = True
+                break
+        assert tag_attached
+    elif tag_op == "untag":
+        dynamic_id = DynamicID(type='VirtualMachine', id=vm_moid)
+        tag_association.detach(tag_id=tag_id, object_id=dynamic_id)
+        for tag_id in tag_association.list_attached_tags(dynamic_id):
+            if tag_id == tag_id:
+                tag_attached = True
+                break
+        assert tag_attached
+
+def main():
+    if len(sys.argv) != 4:
+        print('Usage: ./tag_op <tag/untag> <tag_name> <vm_name>')
+        sys.exit()
+
+    tag_op = sys.argv[1]
+    tag_id = tags[sys.argv[2]]
+    vm_name = sys.argv[3]
+
+    tag(vm_name, tag_op, tag_id)
+    print('Success.')
+
+if __name__ == '__main__':
+    main()
